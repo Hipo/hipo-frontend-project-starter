b834dd70bcbcebca1a3ef15b49335f053e7c4e0b
edizcelik <mr.edizcelik@gmail.com>
2019-08-23 12:17:03 +0300

    Improve authenticationApi example

--- src/authentication/redux/authenticationState.ts
+++ src/authentication/redux/authenticationState.ts
@@ -1,4 +1,4 @@
-import {UserProfileModel} from "../model/authenticationEndpointModels";
+import {UserProfileModel} from "../api/authenticationApiModels";
 
 export type TAuthenticationState = ReturnType<typeof generateInitialAuthenticationState>;
 export type TAuthenticatedProfile = TAuthenticationState["authenticatedProfile"];
