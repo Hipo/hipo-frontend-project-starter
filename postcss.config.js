d3f9f007049721c97100342091e79476bdc338a6
Mucahit Tutuncu <tutuncumucahit@gmail.com>
2020-07-23 18:16:21 +0300

    chore(postcss): Add `postcss-inline-svg`

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 const postcssNormalize = require("postcss-normalize");
 const autoprefixer = require("autoprefixer");
+const postcssInlineSvg = require("postcss-inline-svg");
 
 module.exports = {
   plugins() {
-    return [postcssNormalize(), autoprefixer];
+    return [postcssNormalize(), postcssInlineSvg(), autoprefixer];
   }
 };
